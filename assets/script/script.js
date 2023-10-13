cf532514c002682d5f1548b601f57ad5b6b8309f
Kellinton <kellintonbrasil@gmail.com>
2023-10-13 16:02:31 -0300

    up

---
+++ assets/script/script.js
@@ -0,0 +1,6 @@
+function clicarON(){
+    document.getElementById('imagem').src='assets/img/light-on.png';
+}
+function clicarOFF(){
+    document.getElementById('imagem').src='assets/img/light-off.png';
+}
